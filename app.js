6206089282d8fad5aee48e1b8a978bba079f0618
Anthony <anthony.cattet@gmail.com>
2022-06-09 11:21:46 -0400

    server routes

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ var websocket = require("websocket").server;
 var http = require("http");
 var path = require("path")
 var fs = require("fs");
-const mysql = require('mysql2');
-const cTable = require("console.table");
-const db = require("./db/connection.js");
-const inquirer = require("inquirer");
+const db = require("./config/connection.js");
+//server
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize('chat_like_its_2010', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+  
+  module.exports = sequelize;
 
 // local variables:
 var express=require("express");
