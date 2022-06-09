3d11647c5cfe263ada4cfaddfbea4253c2e7deb6
Auzzie1790 <Auzzie1790@gmail.com>
2022-06-08 21:36:19 -0400

    added home route

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 // include dependencies:
 var websocket = require("websocket").server;
 var http = require("http");
-
-
+var path = require("path")
+var fs = require("fs");
 // local variables:
 var username = "You";
 const port = process.env.PORT || 3001;
 
 
 // create server, and have listen on port 3001:
-var server = http.createServer();
+var server = http.createServer(
+    function(req, res) {
+       console.log(req.url)
+    if(req.url==="/") {
+      //  res.sendFile(path.join(__dirname,'index.html'))
+      fs.readFile(path.join(__dirname,'index.html'),function(err, data) {
+        res.writeHead(200, {
+            'Content-Type':'text/html'
+        })
+        res.write(data)
+        res.end()
+      })
+    }
+    else {
+      console.log('req.url wrong')
+      res.end()
+}}
+);
 
 server.listen(port, function() {
     console.log("Server listening on port " + port);
