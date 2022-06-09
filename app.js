c73f7f9f8260077ff8ddaa2ab780b5eb72d068d4
Jonathan Cushing <firex42069@gmail.com>
2022-06-09 17:01:09 -0400

    some fixes, including removing a lot of duplicated code. Server still can't be started yet since there's no models folder

--- app.js
+++ app.js
@@ -2,62 +2,37 @@
 var websocket = require("websocket").server;
 var http = require("http");
 var path = require("path")
-var fs = require("fs");
-const db = require("./config/connection.js");
 //server
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connection.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
-
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('chat_like_its_2010', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-  
-  module.exports = sequelize;
-
-// local variables:
-var express=require("express");
-var username = "You";
-const port = process.env.PORT || 3001;
-
-const app = express();
 // create server, and have listen on port 3001:
 var server = http.createServer( app
 
 );
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({
     extended:true
-}))
-app.use(express.static('public'))
+}));
+app.use(express.static('public'));
+// turn on routes
+app.use(routes);
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'public/index.html'))
-})
-server.listen(port, function() {
-    console.log("Server listening on port " + port);
+});
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port ' + PORT));
 });
 
 var ws_server = new websocket({
     httpServer: server
 });
-
 
 // on server request, send message:
 ws_server.on("request", function(req) {
--- db/schema.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS chat_like_its_2010;
-
-CREATE DATABASE chat_like_its_2010;
-
