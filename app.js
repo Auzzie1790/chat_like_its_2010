2740db409428ed580a808987a9e093dd9f9258b7
Auzzie1790 <Auzzie1790@gmail.com>
2022-06-08 21:51:18 -0400

    implemented heroku

--- app.js
+++ app.js
@@ -4,30 +4,23 @@ var http = require("http");
 var path = require("path")
 var fs = require("fs");
 // local variables:
+var express=require("express");
 var username = "You";
 const port = process.env.PORT || 3001;
 
-
+const app = express();
 // create server, and have listen on port 3001:
-var server = http.createServer(
-    function(req, res) {
-       console.log(req.url)
-    if(req.url==="/") {
-      //  res.sendFile(path.join(__dirname,'index.html'))
-      fs.readFile(path.join(__dirname,'index.html'),function(err, data) {
-        res.writeHead(200, {
-            'Content-Type':'text/html'
-        })
-        res.write(data)
-        res.end()
-      })
-    }
-    else {
-      console.log('req.url wrong')
-      res.end()
-}}
-);
+var server = http.createServer( app
 
+);
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+app.use(express.static('public'))
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'public/index.html'))
+})
 server.listen(port, function() {
     console.log("Server listening on port " + port);
 });
